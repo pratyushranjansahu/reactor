683182e44582edb962137ca9dc4064754a0646b2
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2021-12-30 19:22:39 +0530

    Add files via upload

--- src/service/FluxAndMonoGeneratorService.java
+++ src/service/FluxAndMonoGeneratorService.java
@@ -3,11 +3,13 @@ package com.learnreactiveprogramming.service;
 import com.learnreactiveprogramming.exception.ReactorException;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
@@ -264,6 +266,59 @@ public class FluxAndMonoGeneratorService {
                 })
                 .onErrorReturn("abc")
                 .log();
+    }
+    public Flux<Integer> explore_generate(){
+        return Flux.generate(()->1,(state,sink) ->{
+            sink.next(state*2);
+            if(state == 10){
+                sink.complete();
+            }
+            return state+1;
+        });
+    }
+    public static List<String> names(){
+        delay(1000);
+        return List.of("alex", "ben", "chloe");
+    }
+    public Flux<String> explore_create(){
+        return Flux.create(sink ->{
+            /*names()
+                    .forEach(sink::next);*/
+            CompletableFuture
+                    .supplyAsync(()->names())
+                            .thenAccept(names ->{
+                                names.forEach(sink :: next);
+                            })
+                            .thenRun(()->sendEvents(sink));
+
+        });
+    }
+
+    public void sendEvents(FluxSink<String> sink) {
+
+        CompletableFuture
+                .supplyAsync(() -> names())
+                .thenAccept(names -> {
+                    names.forEach(name ->{
+                        sink.next(name);
+                        //sink.next(name);
+                    });
+                })
+                .thenRun(sink::complete);
+
+    }
+    public Flux<String> explore_handle(){
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .handle((name,sink)->{
+                    if(name.length()>3){
+                        sink.next(name.toUpperCase());
+                    }
+                });
+    }
+    public Mono<String> explore_create_mono(){
+        return Mono.create(sink ->{
+            sink.success("alex");
+        });
     }
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoSchedulersService = new FluxAndMonoGeneratorService();
